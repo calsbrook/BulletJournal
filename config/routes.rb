1a6b6585141448622fce1e8c02f92103412955cc
Cameron Alsbrook <CameronAlsbrook@gmail.com>
2018-01-12 16:52:57 -0800

    log in works

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  get '/login', to 'sessions#new'
+  get '/login', to: 'sessions#new'
 end
