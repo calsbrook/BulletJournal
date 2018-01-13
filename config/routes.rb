26119ea3b2f0d0c2df96a1ec0fb3472c8fec0845
Cameron Alsbrook <CameronAlsbrook@gmail.com>
2018-01-12 16:47:02 -0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'users#index'
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login', to 'sessions#new'
+end
