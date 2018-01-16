7aff927a512b99704fc22f059681cfc6705e004a
Cameron Alsbrook <CameronAlsbrook@gmail.com>
2018-01-16 09:05:02 -0800

    user model done

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,48 @@
+class NotesController < ApplicationController
+    def index
+        @notes = Note.all
+    end
+
+    def show
+        @note = Note.find(params[:id])
+    end
+
+    def new
+        @note = Note.new
+    end
+
+    def create
+        @note = Note.new
+    
+        respond_to do |format|
+          if @note.save
+            format.html { redirect_to @note, notice: 'Note was successfully created.' }
+            format.json { render :show, status: :created, location: @note }
+          else
+            format.html { render :new }
+            format.json { render json: @note.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+    def destroy
+        @note = Note.find(params[:id])
+        @note.destroy
+        flash[:notice] = "note deleted"
+        redirect_to notes_path
+    end
+
+    def edit
+        @note = Note.find(params[:id])
+    end
+
+    def update
+        @note = Note.find(params[:id])
+
+        if @note.update_attributes(params.require(:note).permit(:handle, :content))
+            redirect_to notes_path
+        else
+            render :edit
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :notes
   get '/login', to: 'sessions#new'
 end
