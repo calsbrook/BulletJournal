f122f568e8fee4fb569d1dd969c1a781bd255fbc
Cameron Alsbrook <CameronAlsbrook@gmail.com>
2018-01-18 09:01:17 -0800

    habit tracker almost complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
   get '/notes/week', to: 'notes#week'
   get '/notes/month', to: 'notes#month'
+  get '/habits/month', to: 'habits#month'
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   resources :notes
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Habit.destroy_all
+habits = Habit.create([
+    {name: "drink water", dates: [Date.today, Date.yesterday, Date.tomorrow], color: 'blue', user_id: 1 },
+    {name: "run", dates: [Date.today], color: 'red', user_id: 1 },
+    {name: "play video games", dates: [Date.today, Date.yesterday, Date.tomorrow], color: 'orange', user_id: 1 }
+])
